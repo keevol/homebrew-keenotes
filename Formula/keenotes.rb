df1492ada7e3a99867bedfdcad0a8f5f766b6f72
afoo <afoo@afoo.me>
2021-02-04 09:14:25 +0800

    setup a mock Formula

--- Formula/keenotes.rb
+++ Formula/keenotes.rb
@@ -5,14 +5,14 @@
 
 # this is for cli install
 
-# class KeeNotes < Formula
-#   desc "A Privacy-First Personal Knowledgebase Tool "
-#   homepage "https://github.com/keevol/keenotes-desktop"
-#   url "https://github.com/keevol/keenotes-desktop/releases/download/v1.2.2/KeeNotes.Desktop-1.2.2.dmg"
-#   sha256 "e3731d376958ba68e927f876b27ade5a9d049461d59af2c4c32dc0df6db12a75"
-#   version "1.2.2"
+class KeeNotes < Formula
+  desc "A Privacy-First Personal Knowledgebase Tool "
+  homepage "https://github.com/keevol/keenotes-desktop"
+  url "https://github.com/keevol/keenotes-desktop/releases/download/v1.2.2/KeeNotes.Desktop-1.2.2.dmg"
+  sha256 "e3731d376958ba68e927f876b27ade5a9d049461d59af2c4c32dc0df6db12a75"
+  version "1.2.2"
 
-#   def install
-#     bin.install "keenotes"
-#   end
-# end
+  def install
+    bin.install "keenotes"
+  end
+end
