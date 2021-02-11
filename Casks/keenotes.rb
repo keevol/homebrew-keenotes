edd76de2e5f2ea6674ba439fe430497818f39c2a
afoo <afoo@afoo.me>
2021-02-11 16:21:20 +0800

    update cask tap

--- Casks/keenotes.rb
+++ Casks/keenotes.rb
@@ -1,23 +1,20 @@
 cask "keenotes" do
-  version "1.2.2"
-  sha256 "e3731d376958ba68e927f876b27ade5a9d049461d59af2c4c32dc0df6db12a75"
+  version "1.2.3"
+  sha256 "f851f8bc965296dc0c4796dc53cd6e3dc0a4534856c268f8ed2f1de8b7aca9b2"
+  url "https://github.com/keevol/keenotes-desktop/releases/download/v1.2.3/KeeNotes.Desktop-1.2.3.dmg"
 
-  url "https://github.com/keevol/keenotes-desktop/releases/download/v1.2.2/KeeNotes.Desktop-1.2.2.dmg"
-  # appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
   name "keenotes"
   desc "A Privacy-First Personal Knowledgebase Tool"
   homepage "https://github.com/keevol/keenotes-desktop"
 
   auto_updates true
-  # depends_on macos: ">= :high_sierra"
 
   app "KeeNotes Desktop.app"
-
 
   postflight do
     # remove quarantine against the application: `sudo xattr -d com.apple.quarantine /Applications/KeeNotes\ Desktop.app`
     system_command '/usr/bin/xattr',
-                   args: ['-d','com.apple.quarantine', "#{appdir}/KeeNotes Desktop.app"],
+                   args: ['-d', 'com.apple.quarantine', "#{appdir}/KeeNotes Desktop.app"],
                    sudo: true
   end
 end
